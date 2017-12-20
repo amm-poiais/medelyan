f9e586278503f2b557b762c6f927dc3637ff62f0
Sergey Medelyan <fakeAcc18@yandex.ru>
2017-12-20 21:19:05 +0300

    Added new models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    # idk how to create a single unique constraint in django, but i do need
+    # to have nick_name unique - hence primary_key=true
+    # for non-registered users nick_name will probably equal to their ip
+    nick_name = models.CharField(max_length=30, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    # this one is not good but let's pretend that it is super-encrypted password hash or somewhat like that;)
+    password = models.CharField(max_length=30)
+
+
+class Question(models.Model):
+    # we really need to know who created the question
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('published')
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50)
+
+    # this one must be auto-calculated
+    votes = models.IntegerField('votes total', default=0)
+
+
+class UserQuestionAnswer(models.Model):
+    # Need to check data uniqueness when user tries to vote - to prohibit multiply
+    # votes on the same question (i.e. just check that the user hasn't voted for this question yet)
+    # Thus, multiple choice is allowed in one vote request.
+
+    # who voted
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    # what was the question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # what was the answer
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
